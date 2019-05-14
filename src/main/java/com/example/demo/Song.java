de75889879b6a4c4900869fb3f10d151f30af6dc
GBTC408013UR <GBTC408013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-14 10:44:38 -0400

    message/description you want

--- src/main/java/com/example/demo/Song.java
+++ src/main/java/com/example/demo/Song.java
@@ -15,4 +15,44 @@ public class Song {
     public void setAlbum(String album) {
         this.album = album;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
